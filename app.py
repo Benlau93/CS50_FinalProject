430800e470b77642d223c3e592a2a3177ba43dd2
Benlau93 <39236266+Benlau93@users.noreply.github.com>
2020-07-25 09:41:36 +0800

    Done back-end

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
-
+import toxic_processing as process
 from flask import Flask, render_template, request, redirect, session, url_for
 app = Flask(__name__)
 
@@ -20,7 +20,16 @@ def index():
 def result():
 	if request.method=="POST":
 		test = request.form.get("toxic-text")
-		return render_template("result.html",test = test)
+		# process text
+		test = process.preprocess_text(test)
+		# tokenize
+		test = process.tokenize(test)
+		# get prediction
+		result = process.predict(test)
+		# additional parameters
+		max_prob = max(result.values())
+		toxic = max_prob >50
+		return render_template("result.html",result = result,toxic=toxic,max_prob = max_prob)
 
 if __name__ == '__main__':
     import os
